06a2a7b91948fdc573c2f559d1cd70e1733545ea
Pedro Martinez <pmartinez1085@gmail.com>
2016-11-15 19:28:05 +0100

    mypage v1

---
+++ resources/views/mygithubreports.blade.php
@@ -0,0 +1,23 @@
+
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+    Pàgina de Pedro
+@endsection
+
+
+@section('main-content')
+    <div class="container-fluid spark-screen">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Xuxman's Github Repos</div>
+
+                    <div class="panel-body">
+                        Hi all, I'm Xuxman and this is My GitHub repos
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
